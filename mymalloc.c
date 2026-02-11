c6286a4e6093e63dacf6b20a654276e0013908e1
rafzal2020 <rafzal2014@gmail.com>
2026-02-11 16:08:18 -0500

    updated mymalloc.c

--- mymalloc.c
+++ mymalloc.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "mymalloc.h"
 
 #define MEMLENGTH 4096
@@ -6,3 +7,18 @@ static union {
 	char bytes[MEMLENGTH];
 	double not_used;
 } heap;
+
+int align_bytes(size_t size) {
+	if (size % 8 != 0) {
+                size = (size % 8) + size;
+        }
+	return size;
+}
+
+void *mymalloc(size_t size, char *file, int line) {
+	size = align_bytes(size);	
+}
+
+void myfree(void *ptr, char *file, int line) {
+	
+}
