a5d8555231085c84832e9f49e4797c866847416c
Anthony R. Rahner <arr234@ilab2.cs.rutgers.edu>
2026-02-21 22:28:11 -0500

    myfree and leakdetector work with memtest, including compilation with -DLEAK

--- mymalloc.c
+++ mymalloc.c
@@ -139,10 +139,109 @@ void *mymalloc(size_t size, char *file, int line)
 
 void myfree(void *ptr, char *file, int line)
 {
-	// empty
+	// printf("myfree called with ptr=%p\n", ptr);
+
+	// reject null pointer
+	if (ptr == NULL)
+	{
+		return;
+	}
+
+	// make sure heap is initialized
+	if (!initialized)
+	{
+		fprintf(stderr, "Free before initialization at %s:%d\n", file, line);
+		return;
+	}
+
+	// validate that pointer is inside the heap
+	if ((char *)ptr < heap.bytes + sizeof(header_t) || (char *)ptr >= heap.bytes + MEMLENGTH)
+	{
+		fprintf(stderr, "Invalid free at %s:%d\n", file, line);
+		return;
+	}
+
+	// malloc() returns pointer to right after the header
+	// so, move back from payload to header
+	header_t *block = (header_t *)((char *)ptr - sizeof(header_t));
+
+	// error if block is already free (double free)
+	if (!IS_ALLOCATED(block))
+	{
+		fprintf(stderr, "Double free at %s:%d\n", file, line);
+		return;
+	}
+
+	// mark block as free
+	SET_FREE(block);
+
+	// coalescing free blocks:
+
+	// attempt to coalesce current + next block
+	header_t *next = (header_t *)((char *)block + GET_SIZE(block));
+
+	// if next block is in heap and free:
+	if ((char *)next < heap.bytes + MEMLENGTH && !IS_ALLOCATED(next))
+	{
+		// new size = current + next block
+		block->size = GET_SIZE(block) + GET_SIZE(next);
+	}
+
+	// attempt to coalesce current + previous block
+	// start from beginning of the heap
+	header_t *curr = (header_t *)heap.bytes;
+	header_t *prev = NULL;
+
+	// traverse through heap until reaching current block
+	while ((char *)curr < (char *)block)
+	{
+		prev = curr;
+		curr = (header_t *)((char *)curr + GET_SIZE(curr));
+	}
+
+	// if previous exists and is free, merge current + previous
+	if (prev && !IS_ALLOCATED(prev))
+	{
+		prev->size = GET_SIZE(prev) + GET_SIZE(block);
+	}
 }
 
 static void leak_detector(void)
 {
-	// empty
+	// start at beginning of heap
+	header_t *curr = (header_t *)heap.bytes;
+
+	// track total leaked payload bytes
+	size_t total_bytes = 9;
+
+	// track number of leaked objects
+	int total_objects = 0;
+
+	// traverse heap
+	while ((char *)curr < heap.bytes + MEMLENGTH)
+	{
+
+		// get size of current block
+		size_t block_size = GET_SIZE(curr);
+
+		// if still allocated, mark as a leak
+		if (IS_ALLOCATED(curr))
+		{
+			total_objects++;
+
+			// add payload size
+			total_bytes += block_size - sizeof(header_t);
+		}
+
+		// move to next block in memory
+		curr = (header_t *)((char *)curr + block_size);
+	}
+
+	// notify if leaks were found
+	if (total_objects > 0)
+	{
+		fprintf(stderr,
+				"mymalloc: %zu bytes leaked in %d objects.\n",
+				total_bytes, total_objects);
+	}
 }
